38704a86deb762999189736d6f5b14d1677f690a
ToniTrum <kulahszjan@gmail.com>
2024-11-29 21:51:15 +1000

    add users.models

--- backend/users/models.py
+++ backend/users/models.py
@@ -2,14 +2,25 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinValueValidator
 
-# class User(AbstractUser):
-#     username = models.CharField(max_length=64, validators=[MinValueValidator(8)], unique=True)
-#     email = models.EmailField(unique=True)
-#     first_name = models.CharField(max_length=64)
-#     last_name = models.CharField(max_length=124)
-#     birth_day = models.DateField(null=False)
-#     country = models.PositiveIntegerField(null=False)
-#     image = models.ImageField(upload_to='media', default='default.svg')
 
-#     def __str__(self):
-#         return self.username
+class User(AbstractUser):
+    username = models.CharField(max_length=64, validators=[MinValueValidator(3)], unique=True)
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return self.username
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=124)
+    birth_day = models.DateField(null=False)
+    country = models.PositiveIntegerField(null=False)
+    image = models.ImageField(upload_to='media', default='default.svg')
+    verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
