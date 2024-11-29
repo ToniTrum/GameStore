f2648e7092b5c5403b7e0711e4530f092f7c532d
ToniTrum <kulahszjan@gmail.com>
2024-11-29 23:20:02 +1000

    add users.serializer

---
+++ backend/users/serializer.py
@@ -0,0 +1,51 @@
+from .models import User, Profile
+from django.contrib.auth.password_validation import validate_password
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
+class TokenSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['username'] = user.username
+        token['email'] = user.email
+
+        token['first_name'] = user.profile.first_name
+        token['last_name'] = user.profile.last_name
+        token['birth_day'] = user.profile.birth_day
+        token['country'] = user.profile.country
+        token['image'] = user.profile.image
+        token['verified'] = user.profile.verified
+
+        return token
+    
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'password2', 'email']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Пароль не соответствует!"})
+
+        return attrs
+    
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
