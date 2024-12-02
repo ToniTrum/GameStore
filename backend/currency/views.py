4c0df19c3e64be8a796b6593a2ff0b2a09acfa17
ToniTrum <kulahszjan@gmail.com>
2024-12-02 23:16:28 +1000

    add currency.views

--- backend/currency/views.py
+++ backend/currency/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from .models import CurrencyRate, Country
+from .serializer import CurrencyRateSerializer, CountrySerializer
+
+@api_view(['GET'])
+def get_currency_rates(request, currency_code):
+    currency_rate = CurrencyRate.objects.get(currency_code=currency_code)
+    serializer = CurrencyRateSerializer(currency_rate)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+def get_country(request):
+    countries = Country.objects.all()
+    serializer = CountrySerializer(countries, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
