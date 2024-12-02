46847c4709c9296cdb779c9c10d59394b16fc359
ToniTrum <kulahszjan@gmail.com>
2024-12-02 23:42:00 +1000

    add currency.urls

---
+++ backend/currency/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("currency_rates/", views.CurrencyRateView.as_view(), name="currency_rates"),
+    path("currency_rates/get/<str:currency_code>/", views.get_currency_rate, name="get_currency_rate"),
+    path("country/", views.CountryView.as_view(), name="country"),
+]
--- backend/currency/views.py
+++ backend/currency/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 from .models import CurrencyRate, Country
 from .serializer import CurrencyRateSerializer, CountrySerializer
 
+class CurrencyRateView(generics.ListAPIView):
+    queryset = CurrencyRate.objects.all()
+    serializer_class = CurrencyRateSerializer
+
 @api_view(['GET'])
-def get_currency_rates(request, currency_code):
+def get_currency_rate(request, currency_code):
     currency_rate = CurrencyRate.objects.get(currency_code=currency_code)
     serializer = CurrencyRateSerializer(currency_rate)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
-def get_country(request):
-    countries = Country.objects.all()
-    serializer = CountrySerializer(countries, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+class CountryView(generics.ListAPIView):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
