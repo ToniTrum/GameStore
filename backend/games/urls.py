50b9228e99bba53d1106d8ddea759e405a58eb8d
ToniTrum <kulahszjan@gmail.com>
2024-12-07 01:46:38 +1000

    add endpoint genre/

--- backend/games/urls.py
+++ backend/games/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     # path("game/", views.GameView.as_view(), name="game"),
     path("platform/", views.PlatformView.as_view(), name="platform"),
     path("esrb_rating/", views.ESRBRatingView.as_view(), name="esrb_rating"),
+    path("genre/", views.GenreView.as_view(), name="genre"),
 ]
