6f92798fed627a8e1316df5e2566428c3af27b65
ToniTrum <kulahszjan@gmail.com>
2024-12-07 01:45:50 +1000

    add GenreView

--- backend/games/views.py
+++ backend/games/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from rest_framework import status, generics
 from rest_framework.response import Response
 
-from .models import Platform, ESRBRating
-from .serializer import PlatformSerializer, ESRBRatingSerializer
+from .models import Platform, ESRBRating, Genre
+from .serializer import PlatformSerializer, ESRBRatingSerializer, GenreSerializer
 
 class PlatformView(generics.ListAPIView):
     queryset = Platform.objects.all()
@@ -12,3 +12,7 @@ class PlatformView(generics.ListAPIView):
 class ESRBRatingView(generics.ListAPIView):
     queryset = ESRBRating.objects.all()
     serializer_class = ESRBRatingSerializer
+
+class GenreView(generics.ListAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
