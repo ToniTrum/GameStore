401906df382a0a84a99d4b597f3f32da7aefeb79
ToniTrum <kulahszjan@gmail.com>
2024-12-07 01:08:22 +1000

    add ESRBRatingView

--- backend/games/views.py
+++ backend/games/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 from rest_framework import status, generics
 from rest_framework.response import Response
 
-from .models import Platform
-from .serializer import PlatformSerializer
+from .models import Platform, ESRBRating
+from .serializer import PlatformSerializer, ESRBRatingSerializer
 
 class PlatformView(generics.ListAPIView):
     queryset = Platform.objects.all()
     serializer_class = PlatformSerializer
+
+class ESRBRatingView(generics.ListAPIView):
+    queryset = ESRBRating.objects.all()
+    serializer_class = ESRBRatingSerializer
