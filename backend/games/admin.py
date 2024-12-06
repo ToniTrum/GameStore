641ca81454d5e0ce200166a83b3bc3f65aa121f5
ToniTrum <kulahszjan@gmail.com>
2024-12-06 22:07:53 +1000

    add PlatformAdmin

--- backend/games/admin.py
+++ backend/games/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Game, Platform
 
-# Register your models here.
+@admin.register(Platform)
+class PlatformAdmin(admin.ModelAdmin):
+    list_display = ('name', 'icon')
