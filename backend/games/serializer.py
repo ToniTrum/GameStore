87a6c58540cfce9b25f85d56aeea29e7391ca4d5
ToniTrum <kulahszjan@gmail.com>
2024-12-07 12:19:02 +1000

    add TagSerializer

--- backend/games/serializer.py
+++ backend/games/serializer.py
@@ -1,4 +1,4 @@
-from .models import Platform, ESRBRating, Genre
+from .models import Platform, ESRBRating, Genre, Tag
 from rest_framework import serializers
 
 class PlatformSerializer(serializers.ModelSerializer):
@@ -14,4 +14,9 @@ class ESRBRatingSerializer(serializers.ModelSerializer):
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
+        fields = ['id', 'name']
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
         fields = ['id', 'name']
