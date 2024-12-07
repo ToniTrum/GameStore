b61b2ee029457d28b6c255b70a817af6a8854757
ToniTrum <kulahszjan@gmail.com>
2024-12-07 16:42:13 +1000

    add url game/

--- backend/games/urls.py
+++ backend/games/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    # path("game/", views.GameView.as_view(), name="game"),
     path("platform/", views.PlatformView.as_view(), name="platform"),
     path("esrb_rating/", views.ESRBRatingView.as_view(), name="esrb_rating"),
     path("genre/", views.GenreView.as_view(), name="genre"),
     path("tag/", views.TagView.as_view(), name="tag"),
+    path("game/", views.GameView.as_view(), name="game"),
 ]
