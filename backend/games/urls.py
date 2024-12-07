5e0b7e1855c5b24a883c97bb3db02885a6232a87
ToniTrum <kulahszjan@gmail.com>
2024-12-07 12:21:19 +1000

    add url tag/

--- backend/games/urls.py
+++ backend/games/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("platform/", views.PlatformView.as_view(), name="platform"),
     path("esrb_rating/", views.ESRBRatingView.as_view(), name="esrb_rating"),
     path("genre/", views.GenreView.as_view(), name="genre"),
+    path("tag/", views.TagView.as_view(), name="tag"),
 ]
