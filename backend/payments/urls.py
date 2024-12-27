c7a24e8597cb8f4cfaa6823fe0d611bfa4f32157
ToniTrum <kulahszjan@gmail.com>
2024-12-28 00:12:11 +1000

    delete test url

--- backend/payments/urls.py
+++ backend/payments/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("test_payment/", views.test_payment, name="test_payment"),
     path("save_stripe_info/", views.save_stripe_info, name="save_stripe_info"),
 ]
