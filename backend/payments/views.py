ac87b8702e8957a0d359850b1b76edb16b71b88d
ToniTrum <kulahszjan@gmail.com>
2024-12-27 21:09:35 +1000

    update save_stripe_info

--- backend/payments/views.py
+++ backend/payments/views.py
@@ -8,24 +8,31 @@ from rest_framework import status
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
 @api_view(['POST'])
-def test_payment(request):
-    test_payment_intent = stripe.PaymentIntent.create(
-        amount=1000, currency='pln',
-        payment_method_types=['card'],
-        receipt_email='test@example.com')
-    return Response(status=status.HTTP_200_OK, data=test_payment_intent)
-
-@api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def save_stripe_info(request):
     data = request.data
     email = data['email']
     payment_method_id = data['payment_method_id']
+    extra_msg = ''
+    customer_data = stripe.Customer.list(email=email).data
     
-    customer = stripe.Customer.create(
-    email=email, payment_method=payment_method_id)
+    if len(customer_data) == 0:
+        customer = stripe.Customer.create(
+        email=email, payment_method=payment_method_id)
+    else:
+        customer = customer_data[0]
+        extra_msg = "Customer already existed."
+    
+    stripe.PaymentIntent.create(
+        customer=customer, 
+        payment_method=payment_method_id,  
+        currency='usd',
+        amount=999,
+        confirm=True,
+        return_url='http://localhost:5173/user/id/16/payment'
+    )
 
-    return Response(status=status.HTTP_200_OK, data={
-        'message': 'Success',
-        'data': {'customer_id': customer.id}
-    }) 
+    return Response(status=status.HTTP_200_OK, 
+        data={'message': 'Success', 'data': {
+        'customer_id': customer.id, 'extra_msg': extra_msg}
+    })
