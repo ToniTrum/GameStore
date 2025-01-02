8907f045a3b4435cb3cf0c324d6c5fc856258d4d
ToniTrum <kulahszjan@gmail.com>
2025-01-02 14:29:23 +1000

    fix token refresh

--- frontend/src/utils/useAxios.js
+++ frontend/src/utils/useAxios.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { jwtDecode } from 'jwt-decode';
 import dayjs from 'dayjs';
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import AuthContext from '../context/AuthContext.jsx';
 import { API_URL } from '../main.jsx';
@@ -32,6 +33,19 @@ const useAxios = () => {
             request.headers.Authorization = `Bearer ${response.data.access}`
         }
         return request;
+    },
+    (error) => {
+        if (error.response?.status === 401 && error.response?.data?.code === "token_not_valid") {
+            console.error("Refresh token is blacklisted. User needs to reauthenticate.");
+            localStorage.removeItem("authTokens");
+            setAuthTokens(null);
+            setUser(null);
+
+            // Перенаправление на страницу логина при истечении срока действия токена
+            navigator = useNavigate();
+            navigator('/login');
+        }
+        return Promise.reject(error);
     })
 
     return axiosInstance
