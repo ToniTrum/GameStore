a9176dbea422ca2c0cb01c2d262cf93561786934
ToniTrum <kulahszjan@gmail.com>
2025-01-10 21:11:41 +1000

    add useFeedbackValidation

---
+++ frontend/src/utils/useFeedbackValidation.js
@@ -0,0 +1,35 @@
+export const validateFeedback = (name, value) => {
+    let error = ""
+
+    switch (name) {
+        case "theme":
+            const themeRegExp = /^(?!.*(^\s|\s$)).{1,128}$/;
+            if (!themeRegExp.test(value)) {
+                error =
+                'Тема не может начинаться и заканчиваться на пробел или табуляцию, а также должна иметь длину от 1 до 128 символов';
+            }
+            break;
+        case "text":
+            const textRegExp = /^(?!.*(^\s|\s$)).{1,}$/;
+            if (!textRegExp.test(value)) {
+                error =
+                'Текст не может начинаться и заканчиваться на пробел или табуляцию, а также не должен быть пустым';
+            }
+            break;
+    }
+
+    return error;
+}
+
+export const useFeedbackValidation = (fieldsToValidate) => {
+    const errors = {}
+
+    fieldsToValidate.forEach(({ name, value }) => {
+        const error = validateFeedback(name, value)
+        if (error) {
+            error[name] = error;
+        }
+    })
+
+    return errors
+}
