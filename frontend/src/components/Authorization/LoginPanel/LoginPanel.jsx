b5b3f8191cd694d406603da3a46907cf994432c8
t-anastasiia <nastaytalmazan@gmail.com>
2025-01-15 13:15:36 +0700

    adaptive auth

--- frontend/src/components/Authorization/LoginPanel/LoginPanel.jsx
+++ frontend/src/components/Authorization/LoginPanel/LoginPanel.jsx
@@ -1,61 +1,71 @@
-import  {useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import AuthContext from "../../../context/AuthContext";
 
-import "../Authorization.css"
+import "../Authorization.css";
 
 const LoginPanel = () => {
-    const navigate = useNavigate()
-    const {loginUser} = useContext(AuthContext)
+  const navigate = useNavigate();
+  const { loginUser } = useContext(AuthContext);
 
-    const onClick = (ref) => {
-        navigate(ref)
+  const onClick = (ref) => {
+    navigate(ref);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+
+    if (email.length > 0 && loginUser(email, password)) {
+      console.log(email);
     }
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        const email = event.target.email.value;
-        const password = event.target.password.value;
+  return (
+    <main>
+      <form
+        className="authorization-form"
+        action=""
+        method="post"
+        onSubmit={handleSubmit}
+      >
+        <h1 className="form-title">Вход</h1>
 
-        if (email.length > 0 && loginUser(email, password)) {
-            console.log(email)
-        }
-    }
+        <div className="form-item">
+          <label className="form-label" htmlFor="email">
+            Электронная почта
+          </label>
+          <input className="form-input" type="email" name="email" id="email" />
+        </div>
 
-    return (
-        <main>
-            <form className="authorization-form" action='' method="post" onSubmit={handleSubmit}>
-                <h1 className="form-title">Вход</h1>
+        <div className="form-item">
+          <label className="form-label" htmlFor="password">
+            Пароль
+          </label>
+          <input
+            className="form-input"
+            type="password"
+            name="password"
+            id="password"
+          />
+        </div>
 
-                <div className="form-item">
-                    <label className="form-label" htmlFor="email">Электронная почта</label>
-                    <input className="form-input" type="email" name="email" id="email" />
-                </div>
+        <div className="form-buttons">
+          <button
+            type="button"
+            onClick={() => onClick("/reset-password/email")}
+          >
+            Забыли пароль?
+          </button>
+          <button type="button" onClick={() => onClick("/register")}>
+            Зарегистрироваться
+          </button>
+          <button type="submit">Войти</button>
+        </div>
+      </form>
+    </main>
+  );
+};
 
-                <div className="form-item">
-                    <label className="form-label" htmlFor="password">Пароль</label>
-                    <input className="form-input" type="password" name="password" id="password" />
-                </div>
-
-                <div className="form-buttons">
-                    <button
-                        type="button"
-                        onClick={() => onClick('/reset-password/email')}>
-                            Забыли пароль?
-                    </button>
-                    <button 
-                        type="button"
-                        onClick={() => onClick('/register')}>
-                            Зарегистрироваться
-                    </button>
-                    <button 
-                        type="submit">
-                            Войти
-                    </button>
-                </div>
-            </form>
-        </main>
-    )
-}
-
-export default LoginPanel
+export default LoginPanel;
