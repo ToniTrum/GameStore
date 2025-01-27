1d3c92460b96ee95b2523016475fedea6c63b5b3
t-anastasiia <nastaytalmazan@gmail.com>
2025-01-27 18:32:17 +1000

    fixed padding payment page

--- frontend/src/components/FeedbackPage/FeedbackPage.jsx
+++ frontend/src/components/FeedbackPage/FeedbackPage.jsx
@@ -1,160 +1,177 @@
-import { useRef, useState } from "react"
-import { useParams, useNavigate } from "react-router-dom"
-import sweetAlert from "sweetalert2"
+import { useRef, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import sweetAlert from "sweetalert2";
 
-import useAxios from "../../utils/useAxios"
-import { validateFeedback } from "../../utils/useFeedbackValidation"
+import useAxios from "../../utils/useAxios";
+import { validateFeedback } from "../../utils/useFeedbackValidation";
 
-import "./FeedbackPage.css" 
+import "./FeedbackPage.css";
 
 const FeedbackPage = () => {
-    const api = useAxios()
-    const navigate = useNavigate()
-    const { id } = useParams()
+  const api = useAxios();
+  const navigate = useNavigate();
+  const { id } = useParams();
 
-    const [theme, setTheme] = useState("")
-    const [text, setText] = useState("")
-    const [errors, setErrors] = useState({
-        theme: "",
-        text: ""
-    })
+  const [theme, setTheme] = useState("");
+  const [text, setText] = useState("");
+  const [errors, setErrors] = useState({
+    theme: "",
+    text: "",
+  });
 
-    const fileRef = useRef(null)
-    const feedbackData = [
-        {
-            name: "theme",
-            stateFunction: setTheme,
-            error: errors.theme
-        },
-        {
-            name: "text",
-            stateFunction: setText,
-            error: errors.text
-        }
-    ]
+  const fileRef = useRef(null);
+  const feedbackData = [
+    {
+      name: "theme",
+      stateFunction: setTheme,
+      error: errors.theme,
+    },
+    {
+      name: "text",
+      stateFunction: setText,
+      error: errors.text,
+    },
+  ];
 
-    const sendErrorMessage = (message) => {
-        sweetAlert.fire({
-            title: message,
-            icon: "error",
-            toast: true,
-            timer: 4000,
-            position: 'top-right',
-            timerProgressBar: true,
-            showConfirmButton: false,
-        })
+  const sendErrorMessage = (message) => {
+    sweetAlert.fire({
+      title: message,
+      icon: "error",
+      toast: true,
+      timer: 4000,
+      position: "top-right",
+      timerProgressBar: true,
+      showConfirmButton: false,
+    });
+  };
+
+  const clearForm = () => {
+    setTheme("");
+    setText("");
+    if (fileRef.current) fileRef.current.value = "";
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    const error = validateFeedback(name, value);
+    setErrors((prev) => ({ ...prev, [name]: error }));
+
+    const stateFunction = feedbackData.find(
+      (item) => item.name === name
+    )?.stateFunction;
+    if (stateFunction) {
+      stateFunction(value);
+    }
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const hasNoErrors = Object.values(errors).every((err) => err === "");
+    if (!hasNoErrors) {
+      sendErrorMessage("Пожалуйста, исправьте ошибки в форме");
+      return;
     }
 
-    const clearForm = () => {
-        setTheme("")
-        setText("")
-        if (fileRef.current) fileRef.current.value = ""
+    const file = fileRef.current.files[0];
+
+    if (theme && text) {
+      const formData = new FormData();
+      formData.append("theme", theme);
+      formData.append("text", text);
+      if (file) formData.append("file", file);
+
+      try {
+        await api.post(`/feedback/feedback/create/${id}/`, formData);
+        navigate(`/user/id/${id}/feedback/page/1`);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      sendErrorMessage("Пожалйста, заполните все поля!");
     }
+  };
 
-    const handleChange = (e) => {
-        const { name, value } = e.target
-        const error = validateFeedback(name, value)
-        setErrors((prev) => ({...prev, [name]: error}))
+  return (
+    <section className="feedback-section">
+      <h2>Заполните форму заявления</h2>
 
-        const stateFunction = feedbackData.find(
-            (item) => item.name === name
-        )?.stateFunction
-        if (stateFunction) {
-            stateFunction(value)
-        }
-    }
+      <WarningMassage />
 
-    const onSubmit = async (e) => {
-        e.preventDefault()
+      <form className="feedback-form" method="post" onSubmit={onSubmit}>
+        <div className="feedback-field">
+          <label className="feedback-label">Тема заявления</label>
+          <input
+            type="text"
+            placeholder="Введите тему..."
+            className="feedback-input"
+            onChange={handleChange}
+            name="theme"
+            value={theme}
+          />
+          <div className="form-error">
+            {errors.theme && (
+              <span className="error-message">{errors.theme}</span>
+            )}
+          </div>
+        </div>
 
-        const hasNoErrors = Object.values(errors).every((err) => err === "")
-        if (!hasNoErrors) {
-            sendErrorMessage("Пожалуйста, исправьте ошибки в форме")
-            return
-        }
-            
-        const file = fileRef.current.files[0]
+        <div className="feedback-field">
+          <label className="feedback-label">Текст заявления</label>
+          <textarea
+            placeholder="Введите текст..."
+            className="feedback-text"
+            onChange={handleChange}
+            name="text"
+            value={text}
+          />
+          <div className="form-error">
+            {errors.text && (
+              <span className="error-message">{errors.text}</span>
+            )}
+          </div>
+        </div>
 
-        if (theme && text) {
-            const formData = new FormData()
-            formData.append("theme", theme)
-            formData.append("text", text)
-            if (file) formData.append("file", file)
+        <label className="feedback-label">Файл</label>
+        <input className="feedback-file-input" ref={fileRef} type="file" />
 
-            try {
-                await api.post(`/feedback/feedback/create/${id}/`, formData)
-                navigate(`/user/id/${id}/feedback/page/1`)
-            }
-            catch (error) {
-                console.log(error)
-            }
-        }
-        else {
-            sendErrorMessage("Пожалйста, заполните все поля!")
-        }
-    }
+        <div className="feedback-button-container">
+          <button onClick={clearForm} className="feedback-button" type="reset">
+            Очистить
+          </button>
+          <button className="feedback-button" type="submit">
+            Отправить
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
 
-    return (
-        <section className="feedback-section">
-            <h2>Заполните форму заявления</h2>
-
-            <WarningMassage />
-
-            <form className="feedback-form" method="post" onSubmit={onSubmit}>
-                <div className="feedback-field">
-                    <label className="feedback-label">Тема заявления</label>
-                    <input
-                        type="text" 
-                        placeholder="Введите тему..." 
-                        className="feedback-input"
-                        onChange={handleChange}
-                        name="theme"
-                        value={theme} />
-                    <div className="form-error">{errors.theme && <span className="error-message">{errors.theme}</span>}</div>
-                </div>
-
-                <div className="feedback-field">
-                    <label className="feedback-label">Текст заявления</label>
-                    <textarea 
-                        placeholder="Введите текст..."
-                        className="feedback-text"
-                        onChange={handleChange}
-                        name="text"
-                        value={text} />
-                    <div className="form-error">{errors.text && <span className="error-message">{errors.text}</span>}</div>
-                </div>
-
-                <label className="feedback-label">Файл</label>
-                <input className="feedback-file-input" ref={fileRef} type="file" />
-
-                <div className="feedback-button-container">
-                    <button onClick={clearForm} className="feedback-button" type="reset">Очистить</button>
-                    <button className="feedback-button" type="submit">Отправить</button>
-                </div>
-            </form>
-        </section>
-    )
-}
-
-export default FeedbackPage
+export default FeedbackPage;
 
 const WarningMassage = () => {
-    return (
-        <div className="warning-massage">
-            <p>
-                Пожалуйста, заполните все поля формы.<br />
-                Так же просим вас, чтобы ваше заявление включало в себя лишь
-                полезную и/или важную информацию о ошибках сайта, замеченные вами,
-                пожелания об улучшении и другие важные вопросы. Также просим как
-                можно подробнее описать вашу проблему и прикреплять
-                файлы при необходимости для наибольшей ясности.<br />
-                Просим также в заявлении соблюдать манеры и не выражаться нецензурной 
-                бранью, соблюдать правила этикета и писать только нужную информацию, 
-                будте уважительнее к администраторам и разработчикам сайта.<br />
-                Также, после создания заявления Вы имеет возможность удалить заявление 
-                или отредактировать его до тех пор, пока он не будет взят на рассмотрение.<br />
-                Спасибо за помощь!
-            </p>
-        </div>
-    )
-}
+  return (
+    <div className="warning-massage">
+      <p>
+        Пожалуйста, заполните все поля формы.
+        <br />
+        Так же просим вас, чтобы ваше заявление включало в себя лишь полезную
+        и/или важную информацию о ошибках сайта, замеченные вами, пожелания об
+        улучшении и другие важные вопросы. Также просим как можно подробнее
+        описать вашу проблему и прикреплять файлы при необходимости для
+        наибольшей ясности.
+        <br />
+        Просим также в заявлении соблюдать манеры и не выражаться нецензурной
+        бранью, соблюдать правила этикета и писать только нужную информацию,
+        будте уважительнее к администраторам и разработчикам сайта.
+        <br />
+        Также, после создания заявления Вы имеет возможность удалить заявление
+        или отредактировать его до тех пор, пока он не будет взят на
+        рассмотрение.
+        <br />
+        Спасибо за помощь!
+      </p>
+    </div>
+  );
+};
